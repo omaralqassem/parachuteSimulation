44322c9a4d12ed7481cb17768f654e9561bd49f7
omaralqassem <omar.gamer12323@gmail.com>
2025-06-15 22:40:31 +0300

    files

--- src/main.js
+++ src/main.js
@@ -2,13 +2,6 @@ import * as THREE from 'three';
 
 
 const scene = new THREE.Scene();
-const cubeGeometry = new THREE.BoxGeometry(1,1,1
-)
-const cubeMaterial =  new THREE.MeshBasicMaterial({color:'red'})
-const cubemesh = new THREE.Mesh(
-  cubeGeometry,
-  cubeMaterial
-)
 
 scene.add(cubemesh)
 
@@ -31,14 +24,3 @@ scene.add(light);
 
 
 
-var helicopter;
-var loader = new GLTFLoader();
-loader.load('./helicopter_model/scene.gltf', function (gltf) {
-  helicopter = gltf.scene;
-  helicopter.position.set(0, h0, 0);
-  helicopter.scale.set(15, 15, 15);
-  scene.add(helicopter);
-});
-var spiner = new THREE.Mesh( new THREE.BoxGeometry( 20, 1, 2 ), new THREE.MeshBasicMaterial( { color: 0xCF0333 } ) );
-scene.add(spiner);
-spiner.position.set(2, 7, -50);
